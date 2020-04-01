13c93b4a69ae835c75f0588ac9e43dc247a7874b
Andy Chan <fung199609@gmail.com>
2020-04-01 16:36:56 +0800

    add skill set layout

--- src/screens/MainScreen/components/ProfilePage.js
+++ src/screens/MainScreen/components/ProfilePage.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col, Image, Card } from "react-bootstrap";
 import localData from '../../../assets/data/experience';
 
 class ProfilePage extends Component {
@@ -60,11 +60,33 @@ class ProfilePage extends Component {
         return (
           <Fragment>
             <div style={{ height: 100 }}></div>
-            <span style={{ marginLeft: 20, fontSize: 22 }}>Experiences</span>
+            <div style={{ fontSize: 22, textAlign: 'center' }}>Experiences</div>
             <Container style={{ marginLeft: 20, marginRight: 20 }}>
               {this._renderWorkItem()}
             </Container>
-            {/* <span style={{ marginLeft: 20, fontSize: 22 }}>Education</span> */}
+            <div style={{ fontSize: 22, textAlign: 'center' }}>Skills Overview</div>
+            <Row style={{ marginTop: 20, marginBottom: 80 }}>
+              <Col md={2} xs={1}></Col>
+              <Col md={8} xs={10} style={{ textAlign: 'justify' }}>
+                <span style={{ fontWeight: 300 }}>
+                  A Mobile Apps Developer with an experience iOS and Android background of 2 years.
+                  A wide range of knowledge in meeting different requirements, designing and
+                  maintenance application based on different situation.
+                </span>
+              </Col>
+              <Col md={2} xs={1}></Col>
+            </Row>
+            <Row style={{ paddingLeft: 40, paddingRight: 40 }}>
+              <Col>
+                <Card style={{ height: 250, boxShadow: '1px 4px 8px rgba(0, 0, 0, 0.1)' }}></Card>
+              </Col>
+              <Col>
+                <Card style={{ height: 250, boxShadow: '1px 4px 8px rgba(0, 0, 0, 0.1)' }}></Card>
+              </Col>
+              <Col>
+                <Card style={{ height: 250, boxShadow: '1px 4px 8px rgba(0, 0, 0, 0.1)' }}></Card>
+              </Col>
+            </Row>
           </Fragment>
         );
     }
